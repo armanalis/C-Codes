2afa88fe7b7388ccaacd4b3a7b4fe6c7f3f8efa0
Ali Arman Dai <74292916+armanalis@users.noreply.github.com>
2024-12-24 13:13:36 +0100

    Create Push and DIsplay in Stack.c

---
+++ Push and DIsplay in Stack.c
@@ -0,0 +1,64 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+struct  Stack {
+    int size;
+    int top;
+    int *array;
+};
+
+void createStack(struct Stack *st) {
+
+    printf("Enter size of stack: ");
+    scanf("%d", &st->size);
+    st->top=-1;
+    st->array = (int *)malloc(st->size * sizeof(int));
+
+}
+
+void displayStack(struct Stack st) {
+
+    int i;
+    for (i=st.top; i>=0; i++) {
+        printf("%d", st.array[i]);
+    }
+    printf("\n");
+
+}
+
+void push(struct Stack *st, int x) {
+
+    if (st->top == st->size-1)
+        printf("Stack overflow\n");
+    else {
+        st->top++;
+        st->array[st->top] = x;
+    }
+
+}
+
+int pop(struct Stack *st) {
+
+    int x=-1;
+    if (st->top==-1) {
+        printf("Stack underflow\n");
+    }
+    else {
+        x=st->array[st->top];
+    }
+
+    return x;
+}
+
+int main() {
+
+    struct Stack st;
+    createStack(&st);
+    push(&st, 10);
+    push(&st, 20);
+    push(&st, 30);
+
+    displayStack(st);
+
+    return 0;
+}
